0e0eb1ff9b1882b46bff9651b0c58b5db8f46ed4
Guy de Messimy <gdemessimy@gmail.com>
2020-11-09 15:21:13 +0100

    copy code from codesdbx

---
+++ my-first-use-effect/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css";
+import BlogSection from "./components/BlogSection";
+import Login from "./components/Login";
+
+export default class App extends React.Component {
+  state = {
+    user: ""
+  };
+  handleLogin = userName => {
+    this.setState({ user: userName });
+  };
+  handleLogout = () => {
+    this.setState({ user: "" });
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.user !== this.state.user) {
+      console.log(`User has changed`);
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.user ? (
+          <BlogSection user={this.state.user} onLogout={this.handleLogout} />
+        ) : (
+          <>
+            <Login onSubmit={this.handleLogin} />
+          </>
+        )}
+      </div>
+    );
+  }
+}
